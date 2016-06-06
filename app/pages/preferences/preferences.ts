3c9d74f59fd775c1fc1f32ff9a45bacbf85386f5
kimo5532 <shotbyabel@gmail.com>
2016-06-06 15:25:41 -0700

    refactor preferences

--- app/pages/preferences/preferences.ts
+++ app/pages/preferences/preferences.ts
@@ -1,63 +1,43 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page,NavController} from 'ionic-angular';
 
-/*
-  Generated class for the PreferencesPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Page({
   templateUrl: 'build/pages/preferences/preferences.html',
+  inputs: ['isDisabled']
 })
+
 export class PreferencesPage {
   constructor(public nav: NavController) {
+    
 
-    this.prefsBeforeRide = [{
+    this.passengerPrefs = [{
 
-      preference: "Open Doors"
-    }, {
+        preference: "Open Doors"
+      }, {
         preference: "Greeting"
       }, {
         preference: "Car Temp"
       }, {
         preference: "Luggage Asst."
-      },
-      {
+      }, {
         preference: "Wait 5 mins"
-      },
-      {
-        preference: "TBD"
-      },
-    ]
-
-    this.prefsInCar = [{
-
-      preference: "Music"
-    }, {
+      }, {
+        preference: "Escort"
+      },{
+        preference: "Music"
+      }, {
         preference: "Smoking"
       }, {
         preference: "Charger"
-      }, {
-        preference: "No Convo"
-      },
-      {
-        preference: "Convo Ok"
-      },
-      {
-        preference: "Water"
       },
     ]
 
-    this.prefsDropOff = [{
 
-      preference: "Open Doors"
-    }, {
-        preference: "Escort"
-      }, {
-        preference: "Luggage Asst"
-      }, {
-        preference: "TBD"
-      },
-    ]    
-  }
+}
+//button toggle
+// toggle() {
+//   this.isOn = !this.isOn;
+
+// }
+
+
 }
